e076841f3f7c9d20e4221c3452a972d20aafdb66
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-16 23:52:20 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,46 @@
+class Song
+
+  attr_accessor :name, :artist, :genre
+
+  @@all = []
+
+  def initialize(name, artist = nil, genre = nil)
+    @name = name
+    self.artist = artist
+    self.genre = genre
+  end
+
+  def self.create(name)
+    song = Song.new(name)
+    song.save
+    song
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def artist=(artist)
+    if artist != nil
+      @artist = artist
+      artist.add_song(self)
+    end
+  end
+
+  def genre=(genre)
+    if genre != nil
+      @genre = genre
+      if !genre.songs.include?(self)
+        genre.songs << self
+      end
+    end
+  end
+end
