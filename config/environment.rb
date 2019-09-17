6acd4c0377b466402d24d328351e6145ef5ff01f
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 00:28:40 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,6 +2,19 @@ require 'bundler'
 Bundler.require
 
 module Concerns
+  module Findable
+    def self.find_by_name(name)
+      self.all.detect {|a| a.name == name}
+    end
+
+    def self.find_or_create_by_name(name)
+      a = self.find_by_name(name)
+      if a == nil
+        a = self.create(name)
+      end
+      a
+    end
+  end
 end
 
 require_all 'lib'
