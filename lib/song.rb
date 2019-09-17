f1a5610b6f1bc9d18f486e9c220a5af03cf053db
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 00:13:38 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,4 +47,11 @@ class Song
   def self.find_by_name(name)
     self.all.detect {|song| song.name == name}
   end
+
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    if song == nil
+      song = self.create(name)
+    end
+  end
 end
