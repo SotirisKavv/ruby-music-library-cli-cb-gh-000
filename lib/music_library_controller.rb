8601fc4215a779058bcf77de60472193d5f365f6
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 01:52:54 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -27,8 +27,8 @@ class MusicLibraryController
   end
 
   def list_artists
-    Song.all.sort{|a, b| a.artist.name <=> b.artist.name}.each_with_index do |song, i|
-      puts "#{i+1}. #{song.artist.name}"
+    Artist.all.sort{|a, b| a.name <=> b.name}.each_with_index do |artist, i|
+      puts "#{i+1}. #{artist.name}"
     end
   end
 
