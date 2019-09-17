1f38fedc23513f4d5f3654c6f4d9998dd7cfa810
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 01:19:23 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -7,8 +7,8 @@ class MusicLibraryController
   def call
     puts "Welcome to your music library!"
     puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of your artists in your library, enter 'list songs'."
-    puts "To list all of your genres in your library, enter 'list songs'."
+    puts "To list all of the artists in your library, enter 'list songs'."
+    puts "To list all of the genres in your library, enter 'list songs'."
     answer = ""
     while answer != "exit"
       answer = gets.strip
