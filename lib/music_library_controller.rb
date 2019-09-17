070fe3fbd5275c70254668fa0eaa9315514ff7f2
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 01:15:46 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -5,6 +5,10 @@ class MusicLibraryController
   end
 
   def call
-    puts "Welcome"
+    puts "Welcome to your music library!"
+    answer = ""
+    while answer != "exit"
+      answer = gets.stripe
+    end
   end
 end
