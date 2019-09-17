f6107921ccc103b6f944487d5e1c07b824a8fb8d
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 00:36:22 +0000

    Automatically backed up by Learn

---
+++ lib/music_importer.rb
@@ -0,0 +1,12 @@
+class MusicImporter
+
+  attr_accessor :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def files
+    Dir.entries(@path).select {|f| !File.directory? f}    
+  end
+end
