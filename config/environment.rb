f60f313bd0b5762f7a731918b7252034567a27f6
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 00:29:52 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -3,11 +3,11 @@ Bundler.require
 
 module Concerns
   module Findable
-    def self.find_by_name(name)
+    def find_by_name(name)
       self.all.detect {|a| a.name == name}
     end
 
-    def self.find_or_create_by_name(name)
+    def find_or_create_by_name(name)
       a = self.find_by_name(name)
       if a == nil
         a = self.create(name)
