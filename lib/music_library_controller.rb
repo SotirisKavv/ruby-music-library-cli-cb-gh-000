dd1823bd5ce6469865616bb5864030f2a07d5f5f
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 02:22:18 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -6,17 +6,32 @@ class MusicLibraryController
 
   def call
     puts "Welcome to your music library!"
-    puts "To list all of your songs, enter 'list songs'."
-    puts "To list all of the artists in your library, enter 'list artists'."
-    puts "To list all of the genres in your library, enter 'list genres'."
-    puts "To list all of the songs by a particular artist, enter 'list artist'."
-    puts "To list all of the songs of a particular genre, enter 'list genre'."
-    puts "To play a song, enter 'play song'."
-    puts "To quit, type 'exit'."
-    puts "What would you like to do?"
     answer = ""
     while answer != "exit"
+      puts "To list all of your songs, enter 'list songs'."
+      puts "To list all of the artists in your library, enter 'list artists'."
+      puts "To list all of the genres in your library, enter 'list genres'."
+      puts "To list all of the songs by a particular artist, enter 'list artist'."
+      puts "To list all of the songs of a particular genre, enter 'list genre'."
+      puts "To play a song, enter 'play song'."
+      puts "To quit, type 'exit'."
+      puts "What would you like to do?"
       answer = gets.strip
+
+      case answer
+      when "list_songs"
+        self.list_songs
+      when "list_artists"
+        self.list_artists
+      when "list_genres"
+        self.list_genres
+      when "list_songs_by_artist"
+        self.list_songs_by_artist
+      when "list_songs_by_genre"
+        self.list_songs_by_genre
+      when "play_song"
+        self.play_song
+      end
     end
   end
 
