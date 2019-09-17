0d3280dbd174f53b3557687ec3568f6e6b7447e4
Sotiris Kavvouras <sotos.kavvouras@gmail.com>
2019-09-17 00:47:55 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -9,4 +9,8 @@ class MusicImporter
   def files
     Dir.entries(@path).select {|f| !File.directory? f}
   end
+
+  def import
+    self.files.each {|file| Song.create_from_filename(file)}
+  end
 end
